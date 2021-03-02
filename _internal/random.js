056fbf8f004b321733915d9005cf80522e77b9da
Dustin Hershman <dustinh17@gmail.com>
2021-03-01 23:51:46 -0500

    improve random number

--- _internal/random.js
+++ _internal/random.js
@@ -5,7 +5,7 @@ function randoNumber (max) {
 }
 
 function minRandoNumber (min, max) {
-  return min + Math.random() * (max - min)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function randoLetter () {
