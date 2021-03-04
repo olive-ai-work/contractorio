0ea5eda97e59131ec405a529359cf32879964764
Dustin Hershman <dustinh17@gmail.com>
2021-03-04 17:18:20 -0500

    Were so close to getting seeds to work

--- _internal/random.js
+++ _internal/random.js
@@ -1,21 +1,43 @@
 const { pipe, replace } = require('kyanite')
+const { _curry2, _curry3 } = require('./_curry')
+const alea = require('./alea')
 
-function randoNumber (max) {
-  return Math.floor(Math.random() * max)
+function randoNumber (max, seed) {
+  const nextSeed = alea(seed).quick()
+
+  return [Math.floor(nextSeed * max), nextSeed]
 }
 
-function minRandoNumber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+function minRandoNumber (min, max, seed) {
+  const nextSeed = alea(seed).quick()
+
+  return [Math.floor(nextSeed * (max - min + 1)) + min, nextSeed]
 }
 
-function randoLetter () {
+const getRandomItem = _curry2(function getRandomItem (items, seed) {
+  const [i, nextSeed] = randoNumber(items.length, seed)
+
+  return [
+    items[i],
+    nextSeed
+  ]
+})
+
+function randomPipe (fns, seed) {
+  return fns.reduce(([acc, currSeed], f) => {
+    const [d, fSeed] = f(currSeed)
+
+    acc.push(d)
+
+    return [acc, fSeed]
+  }, [[], seed])
+}
+
+function randoLetter (seed) {
+  const nextSeed = alea(seed).quick()
   const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-  return letters[randoNumber(letters.length)]
-}
-
-function randoProcedureCode (procedureCodes) {
-  return procedureCodes[randoNumber(procedureCodes.length)]
+  return getRandomItem(letters, nextSeed)
 }
 
 function probability (list) {
@@ -43,47 +65,73 @@ function calcAge (month, day, year) {
   return age
 }
 
-function randoDate (format, { months, days, years }) {
-  const [month, day, year] = [
-    months[randoNumber(months.length)],
-    days[randoNumber(days.length)],
-    years[randoNumber(years.length)]
-  ]
+function randoDate (format, { months, days, years }, seed) {
+  const [[month, day, year], nextSeed] = randomPipe([
+    getRandomItem(months),
+    getRandomItem(days),
+    getRandomItem(years)
+  ], seed)
 
-  return {
+  return [{
     date: pipe([
       replace('M', month),
       replace('D', day),
       replace('Y', year)
     ], format),
     age: calcAge(month, day, year)
-  }
+  }, nextSeed]
 }
 
-function randoAddress ({ buildingInfo, streetNames, cityNames, streetNumbers, states, zips }) {
-  return {
-    line1: `${streetNumbers[randoNumber(streetNumbers.length)]} ${streetNames[randoNumber(streetNames.length)]}`,
-    line2: buildingInfo[randoNumber(buildingInfo.length)],
-    zip: zips[randoNumber(zips.length)],
-    state: states[randoNumber(states.length)],
-    city: cityNames[randoNumber(cityNames.length)]
-  }
+function randoAddressLine ({ streetNumbers, streetNames }, seed) {
+  const [acc, nextSeed] = randomPipe([
+    getRandomItem(streetNumbers),
+    getRandomItem(streetNames)
+  ], seed)
+
+  return [
+    `${acc[0]} ${acc[1]}`,
+    nextSeed
+  ]
 }
 
-function randoName ({ firstNames, lastNames }) {
-  return {
-    firstName: firstNames[randoNumber(firstNames.length)],
-    lastName: lastNames[randoNumber(lastNames.length)]
-  }
+function randoAddress ({ buildingInfo, streetNames, cityNames, streetNumbers, states, zips }, seed) {
+  const [line1, lineSeed] = randoAddressLine({ streetNames, streetNumbers }, seed)
+  const [[line2, zip, state, city], nextSeed] = randomPipe([
+    getRandomItem(buildingInfo),
+    getRandomItem(zips),
+    getRandomItem(states),
+    getRandomItem(cityNames)
+  ], lineSeed)
+
+  return [{
+    line1,
+    line2,
+    zip,
+    state,
+    city
+  }, nextSeed]
+}
+
+function randoName (tables, seed) {
+  const [[firstName, lastName], nextSeed] = randomPipe([
+    getRandomItem(tables.firstNames),
+    getRandomItem(tables.lastNames)
+  ], seed)
+
+  return [{
+    firstName,
+    lastName
+  }, nextSeed]
 }
 
 module.exports = {
   minRandoNumber,
   probability,
-  randoAddress,
-  randoDate,
+  randoAddress: _curry2(randoAddress),
+  randoDate: _curry3(randoDate),
   randoLetter,
-  randoName,
-  randoNumber,
-  randoProcedureCode
+  randoName: _curry2(randoName),
+  randoNumber: _curry2(randoNumber),
+  randomPipe,
+  getRandomItem
 }
